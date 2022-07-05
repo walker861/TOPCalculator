f0771f51ffffb7e9e4bc67b455934884a828860f
Brandon Walker <brandonwalker.861@gmail.com>
2022-07-04 21:10:13 -0400

    initial operation functions, clear button works, all operations work once. Need layout improvements, chaining of operations

--- scripts.js
+++ scripts.js
@@ -1,4 +1,34 @@
 'use strict';
+
+const display = document.querySelector('.display');
+display.textContent = 'Waiting for Input';
+
+const btns = document.querySelectorAll('button');
+let num1 = '';
+let num2 = '';
+let operator = '';
+
+// Events
+btns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    if (btn.textContent === '=') {
+      let calculation = operate(operator, +num1, +num2);
+      display.textContent = calculation;
+      calculation = num1;
+    } else if (btn.textContent.toLowerCase() === 'c') {
+      clear();
+    } else if (!isNaN(+btn.textContent) && num1 !== '') {
+      num2 += btn.textContent;
+      display.textContent = +num2;
+    } else if (!isNaN(+btn.textContent)) {
+      num1 += +btn.textContent;
+      display.textContent = +num1;
+    } else if (isNaN(+btn.textContent)) {
+      operator = btn.textContent;
+    }
+  });
+});
+
 function add(num1, num2) {
   let sum = num1 + num2;
   return sum;
@@ -25,17 +55,21 @@ function percent(num1) {
 }
 
 function operate(operator, num1, num2) {
-  if (operator === 'add') {
+  if (operator === '+') {
     return add(num1, num2);
-  } else if (operator === 'subtract') {
+  } else if (operator === '-') {
     return subtract(num1, num2);
-  } else if (operator === 'multiply') {
+  } else if (operator === '*') {
     return multiply(num1, num2);
-  } else if (operator === 'divide') {
+  } else if (operator === '/') {
     return divide(num1, num2);
-  } else if (operator === 'percent') {
+  } else if (operator === '%') {
     return percent(num1, num2);
   }
 }
 
-function clear() {}
+function clear() {
+  num1 = '';
+  num2 = '';
+  display.textContent = 'Waiting for Input';
+}
